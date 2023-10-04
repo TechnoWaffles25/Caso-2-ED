a58b307db2baf48889a7f67a30f5d33adcdedbb7
Alejandro Montero <amontero0525@gmail.com>
2023-10-04 09:46:22 -0600

    Preliminar 2 2

--- clases/class_cajero.h
+++ clases/class_cajero.h
@@ -15,14 +15,14 @@ class Cajero
             // Constructor
         }
         void getCajero(){
-            // Accesa el nombre del mesero
+            // Accesa el nombre del cajero
         }
         Pedido apuntarOrden(Cliente cliente_orden){
             // Atiende a un cliente en la caja SOLO si hay espacio en el vector restaurante
             // Crea el struct y le pone la string item, int num_orden y el bool listo = False
             // El cliente luego pasa al area comun
         }
-        void comunicarOrden(Pedido pedido, Chef chef, queue pedidosPendientes){
+        void comunicarOrden(Pedido pedido, queue pedidosPendientes){
             // Coloca la orden en la cola de ordenes para que un chef la reciba y la cocine
         }
 };
