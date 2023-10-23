44cc8b28f79c23cd7d0bedcc98cfc8ec745158fa
Luis <lpoveda@estudiantec.cr>
2023-10-22 22:15:56 -0600

    xd

--- clases/class_lavaplatos.h
+++ clases/class_lavaplatos.h
@@ -2,16 +2,17 @@
 #include "listas/stack.h"
 #include "class_plato.h"
 #include <iostream>
+#include <stack>
 
-class Lavaplatos
-{
+
+class Lavaplatos {
 private:
-    std::string name; /* Nombre sacado de la lista json */
-    stack platosSucios;
-    stack platosLimpios;
+    std::string name; // Nombre del lavaplatos
+    stack<Plato*> platosSucios;
+    stack<Plato*> platosLimpios;
 
 public:
-    Lavaplatos(std::string pName) : name(pName) {}
+    Lavaplatos(const std::string& pName) : name(pName) {}
 
     std::string getName() {
         return name;
@@ -20,7 +21,7 @@ public:
     // Agarra un plato de la pila de platos sucios, lo limpia y coloca el plato en la pila de platos limpios
     void limpiarPlato() {
         if (!platosSucios.isEmpty()) {
-            Plato* platoSucio = static_cast<Plato*>(platosSucios.pop());
+            Plato* platoSucio = platosSucios.pop();
             // Simular el proceso de lavado
             std::cout << "El lavaplatos " << name << " está lavando el plato " << platoSucio->getName() << std::endl;
             // Cambiar el estado del plato a limpio
@@ -39,7 +40,7 @@ public:
     // Obtener un plato limpio de la pila de platos limpios
     Plato* obtenerPlatoLimpio() {
         if (!platosLimpios.isEmpty()) {
-            return static_cast<Plato*>(platosLimpios.pop());
+            return platosLimpios.pop();
         }
         return nullptr; // La pila de platos limpios está vacía
     }
